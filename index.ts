3a0e400ea7c645b7c9cf4c551bde253b545b7b94
tgorka <tomasz@gorka.org.pl>
2021-10-22 22:04:36 +0000

    devcontaine configuration fixes

--- index.ts
+++ index.ts
@@ -1,3 +1,3 @@
 // Add your pipes here for easy indexing.
-export { DataStoreQuerySuject } from './datastore/datastore.query.suject';
-export { DataStoreGetSuject } from './datastore/datastore.get.suject';
+export { DataStoreQuerySuject } from './src/datastore.query.suject';
+export { DataStoreGetSuject } from './src/datastore.get.suject';
