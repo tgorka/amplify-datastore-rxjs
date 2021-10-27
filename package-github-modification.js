49de4bcf512797c09c526fe43fceec03206d493c
tgorka <tomasz@gorka.org.pl>
2021-10-27 07:37:54 +0000

    change registry info in package.json

--- package-github-modification.js
+++ package-github-modification.js
@@ -1,4 +1,4 @@
-// package-name-change.js
+// package-github-modification.js
 //import { writeFileSync, readFileSync } from "fs";
 const { writeFileSync, readFileSync } = require("fs");
 
@@ -9,5 +9,6 @@ const file = readFileSync("./package.json", {
 const json = JSON.parse(file);
 
 json.name = `${process.env.SCOPE}/${json.name}`;
+json.publishConfig.registry = process.env.REGISTRY;
 
 writeFileSync("./package.json", JSON.stringify(json, undefined, 2));
